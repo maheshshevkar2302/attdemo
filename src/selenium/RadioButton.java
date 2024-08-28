32065e227e2ed2a1a26238127c043ea8f2f30cbe
mahesh@gmail.com <mahesh@gmail.com>
2024-08-28 17:12:54 +0530

    SeleniumPrograms

---
+++ src/selenium/RadioButton.java
@@ -0,0 +1,27 @@
+package selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Webdriver;
+import org.openqa.selenium.Webelement;
+import org.openqa.selemium.Chrome.chromeDriver;
+
+public class RadioButton{
+	
+
+	public static void main(String[] args) {
+		
+System.setProperty(key, value);
+
+WebDriver driver = new ChromeDriver();
+
+driver.mange().window().maximize();
+driver.get();
+
+Thread.sleep(2000);
+
+driver.FindElement
+	}
+
+}
